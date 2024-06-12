8f50ae70df1bc83a4670daf06d4bb3fafc9591cb
sreyalakshmi <sreyalakshmisaji@gmail.com>
2024-06-12 21:31:04 +0530

    updated

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -21,7 +21,7 @@ const Add = () => {
                         </div>
                     </div><br />
                     <div className="row g-3">
-                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                             <button className="btn btn-success">Add</button>
                         </div>
                     </div>
